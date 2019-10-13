57bb4f3815b807cc82551ac9f12093cf0ab4aae5
lowdad <javawcy@outlook.com>
2019-10-13 15:32:58 +0800

    fix bugs

--- config-demo/src/main/java/com/lowdad/dev/configdemo/ConfigDemoApplication.java
+++ config-demo/src/main/java/com/lowdad/dev/configdemo/ConfigDemoApplication.java
@@ -2,8 +2,10 @@ package com.lowdad.dev.configdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigDemoApplication {
 
     public static void main(String[] args) {

83f66289ef9c7932e5d4f35671db5691bb2ea663
lowdad <javawcy@outlook.com>
2019-10-15 16:32:21 +0800

    add prometheus support

--- config-demo/src/main/java/com/lowdad/dev/configdemo/ConfigDemoApplication.java
+++ config-demo/src/main/java/com/lowdad/dev/configdemo/ConfigDemoApplication.java
@@ -1,8 +1,12 @@
 package com.lowdad.dev.configdemo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,9 @@ public class ConfigDemoApplication {
         SpringApplication.run(ConfigDemoApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
